da73deec2134074910f06b8c91fd4a06ce910e4e
pyi7628 <pyi7628@gmail.com>
2020-06-21 06:49:15 +0900

    recommand- page indexing & connect db

--- src/views/Recommand.vue
+++ src/views/Recommand.vue
@@ -56,15 +56,15 @@
 
                     <div class="col-lg-12">
                         <div class="row row-grid">
-                            <div class="col-lg-4">
+                            <div class="col-lg-4" v-for="g in paginatedData()" :key="g.Board_game_ID">
                                
                                 <card class="border-0" hover shadow body-classes="py-5" >
                                     
                                     <!--icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
-                                    </icon--> <img class="card-img" src="img/games/할리갈리.jpg" >
+                                    </icon--> <img class="card-img" :src="g.Img_url" >
                                    <div class="row row-grid">
                                   
-                                    <h4 class="text-primary display-4 text-uppercase" style="margin-left:20px; margin-right:20px;">할리갈리</h4>
+                                    <h4 class="text-primary display-4 text-uppercase" style="margin-left:20px; margin-right:20px;">{{ checkBoardTitle(g.Title) }}</h4>
                           
                                  
                                     <GameRating :grade="5" :maxStars="5" :hasCounter="true" />
@@ -80,87 +80,7 @@
                                     </base-button>
                                 </card>
                             </div>
-                            <div class="col-lg-4">
-                               <card class="border-0" hover shadow body-classes="py-5" >
-                                    
-                                    <!--icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
-                                    </icon--> <img class="card-img" src="img/games/젠가.jpg" >
-                                   <div class="row row-grid">
-                                  
-                                    <h4 class="text-primary display-4 text-uppercase" style="margin-left:20px; margin-right:20px;">젠가</h4>
-                          
-                                 
-                                    <GameRating :grade="1" :maxStars="5" :hasCounter="true" />
-                                
-                                   </div>
-                                    <p class="description mt-3"> 
-                                    <div>
-                                        <badge type="primary" rounded>party game</badge>
-                                    </div>
-                                    <base-button tag="a" href="#" type="primary" class="mt-4">
-                                        More Detail..
-                                    </base-button>
-                                </card>
-                            </div>
-                            <div class="col-lg-4">
-                               <card class="border-0" hover shadow body-classes="py-5" >
-                                    
-                                    <!--icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
-                                    </icon--> <img class="card-img" src="img/games/팽귄얼음깨기.jpg" >
-                                   <div class="row row-grid">
-                                  
-                                    <h4 class="text-primary display-4 text-uppercase" style="margin-left:20px; margin-right:20px;">팽귄 얼음 깨기</h4>
-                          
-                                 
-                                    <GameRating :grade="5" :maxStars="5" :hasCounter="true" />
-                                
-                                   </div>
-                                    <p class="description mt-3"> 
-                                    <div>
-                                        <badge type="primary" rounded>party game</badge>
-                                        <badge type="primary" rounded>penguin</badge>
-                                        <badge type="primary" rounded>crush</badge>
-                                    </div>
-                                    <base-button tag="a" href="#" type="primary" class="mt-4">
-                                        More Detail..
-                                    </base-button>
-                                </card>
-                            </div>
-                            <div class="col-lg-4">
-                                <card class="border-0" hover shadow body-classes="py-5">
-                                    <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
-                                    </icon>
-                                    <h6 class="text-success text-uppercase">Build Something</h6>
-                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
-                                        4 that includes the most important components and features.</p>
-                                    <div>
-
-                                        <badge type="success" rounded>business</badge>
-                                        <badge type="success" rounded>vision</badge>
-                                        <badge type="success" rounded>success</badge>
-                                    </div>
-                                    <base-button tag="a" href="#" type="success" class="mt-4">
-                                        Learn more
-                                    </base-button>
-                                </card>
-                            </div>
-                            <div class="col-lg-4">
-                                <card class="border-0" hover shadow body-classes="py-5">
-                                    <icon name="ni ni-planet" type="warning" rounded class="mb-4">
-                                    </icon>
-                                    <h6 class="text-warning text-uppercase">Prepare Launch</h6>
-                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
-                                        4 that includes the most important components and features.</p>
-                                    <div>
-                                        <badge type="warning" rounded>marketing</badge>
-                                        <badge type="warning" rounded>product</badge>
-                                        <badge type="warning" rounded>launch</badge>
-                                    </div>
-                                    <base-button tag="a" href="#" type="warning" class="mt-4">
-                                        Learn more
-                                    </base-button>
-                                </card>
-                            </div>
+                           
                         </div>
                         
                     </div>
@@ -182,7 +102,7 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 import Rating from '../components/Ratings.vue'
 import GameRating from '../components/GameRating.vue'
 export default {
@@ -197,26 +117,44 @@ export default {
       pagination: {
         currentPage: 1,
         pageSize: 6,
-      }
+        
+      },
+      recommandGames: '',
     }
     
   },
+  async beforeCreate() { //백엔드에서 games 가져오는 rest.
+            const result = await axios.get("/api/games/recommand");
+            this.recommandGames = result.data;
+           // alert(this.recommandGames[1].Title);
+  },
+
   methods:{
-        
+        checkBoardTitle(title) {
+            
+                if (title.length > 6) return title.substring(0, 6) + "...";
+                else return title;
+         },
         getPageIndex(index)
         {
             this.pagination.currentPage=index;
         },
         paginatedData() {
-            const start = this.pagination.currentPage * this.pagination.pageSize,
+            const start = (this.pagination.currentPage-1) * this.pagination.pageSize,
                 end = start + this.pagination.pageSize;
             
-            let result;//get으로 page 크기만큼 보드게임 정보 DB에서 받아오기!!!
+            let result = this.recommandGames.slice(start, end);//get으로 page 크기만큼 보드게임 정보 DB에서 받아오기!!!
             return result;
         }
     },
     
 };
+
+
+
+
+
+///아래 css는 정렬방식 효과 때문에 넣은거임
 </script>
 
 <style>
