7cf383ad027a3f9055189ca45fa869f97284b398
pyi7628 <pyi7628@gmail.com>
2020-06-22 13:39:25 +0900

    recommand page & detail

---
+++ src/views/BoardgameDetail.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="profile-page">
+        <section class="section-profile-cover section-shaped my-0">
+            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+               <div class="container shape-container d-flex">
+                    <div class="col px-3">
+                        <div class="row">
+                            <div class="col-lg-7" style="position: relative; margin-top:-50px;">
+                                <h1 class="display-2  text-white"> Board Game Detail
+                                </h1>
+                               
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                   
+                    
+                </div>
+        </section>
+        <section class="section section-skew">
+            <div class="container">
+                <card shadow class="card-profile mt--300" no-body>
+                    <div class="px-4">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-3 order-lg-2">
+                                <div class="card-profile-image">
+                                    <a href="#">
+                                        <img v-lazy="boardgame.Img_url" class="raised">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
+                                <div class="card-profile-actions py-4 mt-lg-0">
+                                    <base-button type="info" size="sm" class="mr-4">Connect</base-button>
+                                    <base-button type="default" size="sm" class="float-right">Save</base-button>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 order-lg-1">
+                                <div class="card-profile-stats d-flex justify-content-center">
+                                   <!--j> <div>
+                                        <span class="heading">NaN</span>
+                                        <span class="description">Friends</span>
+                                    </div>
+                                    <div>
+                                        <span class="heading">10</span>
+                                        <span class="description">Photos</span>
+                                    </div>
+                                    <div>
+                                        <span class="heading">89</span>
+                                        <span class="description">Comments</span>
+                                    </div>
+                                    <j-->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center mt-3">
+                            <h5 class="game-name">{{boardgame.Title}}
+                            </h5>
+                           </div>
+                           <Rating :grade="0" :maxStars="5" :hasCounter="true"/>
+                        <div class="mt-5 py-5 border-top text-center">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-9">
+                                 <div class="row">
+                                     <div class="col-lg-12 ">
+                                         
+
+                                      
+       
+    
+                                        <base-input placeholder="Review">
+
+                                        </base-input>
+             
+                                     </div>
+           
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </card>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+import Rating from '../components/Ratings.vue'
+import GameRating from '../components/GameRating.vue'
+export default {
+
+name: "review",
+ components: {
+    Rating,
+    GameRating
+  },
+data() {
+    return {
+      
+      boardgame: '',
+      toggleHeart: false,
+      genreList:'',
+
+
+    }
+},
+async beforeCreate() { //백엔드에서 games 가져오는 rest.
+    var id = this.$route.params.idx
+    const genreresult = await axios.get("/api/games/gamegenre/"+id);
+    this.genreList=genreresult.data;
+    const result = await axios.get("/api/games/boardgame/"+id);
+    this.boardgame = result.data;
+
+           
+  },
+
+  method: {
+  async getGenreFromBack()
+    {        
+         
+    },
+  },
+}
+
+</script>
+<style>
+@media all and (min-width:1000px) {
+  .game-name{
+      padding-top: 50px;
+  }
+}
+</style>
